07d03fc16100b4fed6bb5075acc24bd90fa27d44
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-04-13 11:42:45 -0400

    made script to try and keep .darklua.json and settings.json in sync

---
+++ scripts/updateRequires.py
@@ -0,0 +1,48 @@
+# Script to keep .darklua.json bundling and settings.json in sync.
+# Does it really need a script? No, probably not...
+import os, json
+
+VSCODE_SETTINGS_JSON = ".vscode/settings.json"
+REQUIRE_FILES = "requires"
+DARKLUA_JSON = ".darklua.json"
+
+# with open(DARKLUA_JSON, "r") as file:
+#     darklua_dump  = json.dump(file)
+
+def main():
+    requires = os.listdir(REQUIRE_FILES)
+
+    darklua_output = {
+        "bundle": {
+            "require_mode": {
+                "name": "path",
+            },
+            "excludes": []
+        },
+        "rules": []
+    }
+
+    vscode_settings_output = {
+        "luau-lsp.sourcemap.enabled": False,
+        "luau-lsp.types.roblox": True,
+        "luau-lsp.types.definitionFiles": ["./build/pilot.d.lua"],
+        "luau-lsp.require.fileAliases": {
+        }
+    }
+
+    for file_name in requires:
+        darklua_output["bundle"]["excludes"].append(file_name)
+
+        vscode_settings_output["luau-lsp.require.fileAliases"][os.path.splitext(file_name)[0]] = f"requires/{file_name}"
+
+    print(json.dumps(vscode_settings_output, indent=4))
+
+    with open(DARKLUA_JSON, "w") as file:
+        json.dump(darklua_output, file, indent=4)
+
+    with open(VSCODE_SETTINGS_JSON, "w") as file:
+        json.dump(vscode_settings_output, file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
