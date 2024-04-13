2b5c2ec1d622acb5e17837e616646925d3c45b6d
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-04-13 10:31:05 -0400

    added module tween and repr options type

--- requires/repr.lua
+++ requires/repr.lua
@@ -1,4 +1,24 @@
+type options = {
+	commentPointerIds: false,
+	environment: boolean,
+	includeFunctionLocation: false,
+	metatables: boolean,
+	pointerIds: boolean,
+	pretty: boolean,
+	robloxClassName: boolean,
+	robloxFullName: boolean,
+	robloxProperFullName: boolean,
+	robloxShortcutServices: boolean,
+	sanitizeInvalids: false,
+	semicolons: boolean,
+	sortKeys: boolean,
+	spaces: number,
+	stubFunctions: false,
+	tabs: boolean,
+	trailingSeparator: boolean,
+}
+
 --- Returns a pretty printed version of the string
-return function(tble: { [any]: any }): string
+return function(tble: { [any]: any }, options: options?): string
 	return nil :: any
 end
---
+++ requires/tween.lua
@@ -0,0 +1,7 @@
+local tween = {}
+
+function tween:GetValue(alpha: number, easingStyle: Enum.EasingStyle, easingDirection: Enum.EasingDirection): number
+	return nil :: any
+end
+
+return tween
