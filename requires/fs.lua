8ef4f1b4ce42754967f39ab95864a425bc0e9aae
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-04-13 14:44:14 -0400

    added some types to fs methods

--- requires/fs.lua
+++ requires/fs.lua
@@ -9,7 +9,7 @@ type attributes = {
 type fs = {
 	attributes: attributes,
 	kind: "root",
-	pwd: "/",
+	-- pwd: "/",
 	root: {
 		attributes: attributes,
 		contents: {
@@ -24,6 +24,24 @@ type fs = {
 	},
 
 	readdir: (self: fs, path: string) -> { string },
+	-- chdir: (arg1: any, arg2: any) -> (), -- Broken...
+	copy: (self: fs, from: string, to: string) -> (),
+	exists: (arg1: any, arg2: any) -> (),
+	filename: (path: string) -> (),
+	join: (...string) -> string,
+	mkdir: (self: fs, path: string) -> (),
+	mklink: (arg1: any, arg2: any, arg3: any) -> (),
+	moveMerge: (arg1: any, arg2: any, arg3: any) -> (),
+	new: (arg1: any) -> (),
+	parentdir: (path: string) -> string,
+	-- pwd: (arg1: any) -> (),
+	readfile: (self: fs, path: string) -> string,
+	rename: (arg1: any, arg2: any, arg3: any) -> (),
+	resolve: (arg1: any, arg2: any) -> (),
+	separator: "/",
+	split: (path: string) -> { string },
+	unlink: (arg1: any, arg2: any) -> (),
+	writefile: (self: fs, path: string, contents: any) -> (),
 }
 
 return {} :: fs
