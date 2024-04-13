f151b9f550bebcda4b8acc39dfdbdb84ccdb49fa
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-04-13 11:43:44 -0400

    working on filesystem global

--- globals.d.lua
+++ globals.d.lua
@@ -12,3 +12,26 @@ declare JSONEncode: (dataToEncode: {[string]: any}) -> string
 declare Communicate: () -> ()
 
 declare Microcontroller: PilotLuaMicrocontroller
+
+-- TODO: Finish this later...
+--[[ type FileSystem = {
+	chdir: (arg1: any, arg2: any) -> (),
+	copy: (arg1: any, arg2: any, arg3: any) -> (),
+	exists: (arg1: any, arg2: any) -> (),
+	filename: (arg1: any) -> (),
+	join: (...any) -> (),
+	mkdir: (arg1: any, arg2: any) -> (),
+	mklink: (arg1: any, arg2: any, arg3: any) -> (),
+	moveMerge: (arg1: any, arg2: any, arg3: any) -> (),
+	new: (arg1: any) -> (),
+	parentdir: (arg1: any) -> (),
+	pwd: (arg1: any) -> (),
+	readdir: (arg1: any, arg2: any) -> (),
+	readfile: (arg1: any, arg2: any) -> (),
+	rename: (arg1: any, arg2: any, arg3: any) -> (),
+	resolve: (arg1: any, arg2: any) -> (),
+	separator: "/",
+	split: (arg1: any) -> (),
+	unlink: (arg1: any, arg2: any) -> (),
+	writefile: (arg1: any, arg2: any, arg3: any) -> ()
+} ]]
